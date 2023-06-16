abdc99bd97613f4ca168cd0939932fcfddf1cfa9
Aleksey Usynin <fesskagf@yandex.ru>
2023-06-16 15:37:06 +0400

    fix auth.js, add logger.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,23 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
-  // достаём авторизационный заголовок
-  const { authorization } = req.headers;
-  // убеждаемся, что он есть или начинается с Bearer
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Unauthorized('Необходима авторизация!');
+  const token = req.cookies.jwt;
+  if (!token) {
+    return next(new Unauthorized('Необходима авторизация!'));
   }
 
-  // извлечём токен
-  const token = authorization.replace('Bearer ', '');
-  let payload; // за пределами try/catch, так как от туда она будет не доступна
+  let payload;
 
   try {
-    // попытаемся верифицировать токен
-    payload = jwt.verify(token, 'fire');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'fire');
   } catch (err) {
-    // отправим ошибку, если не получилось
-    throw new Unauthorized('Необходима авторизация!');
+    return next(new Unauthorized('Необходима авторизация!'));
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
+  req.user = payload;
 
-  return next(); // пропускаем запрос дальше
+  return next();
 };
